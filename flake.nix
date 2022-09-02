12a48b108ae275a6fc919a9f797c969c31b26cbb
Anish Lakhwara <anish+git@lakhwara.com>
2022-09-02 19:45:28 +1000

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Fork of vimwiki-cli so I can bump the deps lol";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      my-python = pkgs.python3;
+      python-with-my-packages = my-python.withPackages (p: with p; [
+        click
+      ]);
+      python-deps = with pkgs.python3Packages; [
+        click
+        setuptools
+      ];
+    in
+    {
+      packages.x86_64-linux.vimwiki-cli = pkgs.python3Packages.buildPythonPackage {
+        pname = "vimwiki-cli";
+        src = ./.;
+        version = "1.0.0";
+        bulidInputs = [ python-deps  ];
+        propagatedBuildInputs = [ python-deps ];
+        doCheck = false;
+      };
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = [
+          python-with-my-packages
+          self.packages.x86_64-linux.vimwiki-cli
+        ];
+      };
+    };
+}
